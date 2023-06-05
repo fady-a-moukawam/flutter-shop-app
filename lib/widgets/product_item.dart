b6c64a18921cd515175810e726d19d7a7447e957
Fady Moukawam <famoukawam@alinma.com>
2023-06-05 15:10:43 +0300

    chore: new product overview widget

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const ProductItem({
+    super.key,
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black38,
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () => {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () => {},
+          ),
+        ),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
