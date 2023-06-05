7d177308ef8a6cd1bdca79f8ba396468d9332c4b
Fady Moukawam <famoukawam@alinma.com>
2023-06-05 17:38:11 +0300

    feat: ProductGridView created

---
+++ lib/widgets/product_grid_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/widgets/product_item.dart';
+
+class ProductGridView extends StatelessWidget {
+  const ProductGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+
+    return GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: products.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemBuilder: ((context, index) {
+          return ProductItem(
+              id: products[index].id,
+              title: products[index].title,
+              imageUrl: products[index].imageUrl);
+        }));
+  }
+}
