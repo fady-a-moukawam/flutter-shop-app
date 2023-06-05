c3859c5f5548ba6c7bfa60fd7965c6bf9e2c8f9f
Fady Moukawam <famoukawam@alinma.com>
2023-06-05 16:40:07 +0300

    chore: wrap with Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
@@ -13,28 +15,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My shop app',
-      theme: ThemeData(
-          fontFamily: 'Lato',
-          primarySwatch: Colors.blue,
-          colorScheme: const ColorScheme(
-            brightness: Brightness.dark,
-            primary: Colors.blue,
-            onPrimary: Colors.white,
-            secondary: Colors.deepOrange,
-            onSecondary: Colors.white,
-            error: Colors.red,
-            onError: Colors.white,
-            background: Colors.black87,
-            onBackground: Colors.white,
-            surface: Colors.transparent,
-            onSurface: Colors.white,
-          )),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (context) => const ProductDetailScreen()
-      },
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: 'My shop app',
+        theme: ThemeData(
+            fontFamily: 'Lato',
+            primarySwatch: Colors.blue,
+            colorScheme: const ColorScheme(
+              brightness: Brightness.dark,
+              primary: Colors.blue,
+              onPrimary: Colors.white,
+              secondary: Colors.deepOrange,
+              onSecondary: Colors.white,
+              error: Colors.red,
+              onError: Colors.white,
+              background: Colors.black87,
+              onBackground: Colors.white,
+              surface: Colors.transparent,
+              onSurface: Colors.white,
+            )),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (context) =>
+              const ProductDetailScreen()
+        },
+      ),
     );
   }
 }
