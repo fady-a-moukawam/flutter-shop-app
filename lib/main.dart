c037c3d12b426208be13de507f74f85281d19d02
Fady Moukawam <famoukawam@alinma.com>
2023-06-08 15:03:17 +0300

    feat: combine providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/products.dart';
 
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
+
+import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/providers/products.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,32 +17,36 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
-      child: MaterialApp(
-        title: 'My shop app',
-        theme: ThemeData(
-            fontFamily: 'Lato',
-            primarySwatch: Colors.blue,
-            colorScheme: const ColorScheme(
-              brightness: Brightness.dark,
-              primary: Colors.blue,
-              onPrimary: Colors.white,
-              secondary: Colors.deepOrange,
-              onSecondary: Colors.white,
-              error: Colors.red,
-              onError: Colors.white,
-              background: Colors.black87,
-              onBackground: Colors.white,
-              surface: Colors.transparent,
-              onSurface: Colors.white,
-            )),
-        home: const ProductsOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (context) =>
-              const ProductDetailScreen()
-        },
-      ),
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => Products()),
+          ChangeNotifierProvider(
+            create: (context) => Cart(),
+          )
+        ],
+        child: MaterialApp(
+          title: 'My shop app',
+          theme: ThemeData(
+              fontFamily: 'Lato',
+              primarySwatch: Colors.blue,
+              colorScheme: const ColorScheme(
+                brightness: Brightness.dark,
+                primary: Colors.blue,
+                onPrimary: Colors.white,
+                secondary: Colors.deepOrange,
+                onSecondary: Colors.white,
+                error: Colors.red,
+                onError: Colors.white,
+                background: Colors.black87,
+                onBackground: Colors.white,
+                surface: Colors.transparent,
+                onSurface: Colors.white,
+              )),
+          home: const ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (context) =>
+                const ProductDetailScreen()
+          },
+        ));
   }
 }
