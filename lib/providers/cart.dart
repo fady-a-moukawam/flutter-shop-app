fb2f9f39ec6d760ced1960cbdd7249013da70052
Fady Moukawam <famoukawam@alinma.com>
2023-06-08 12:57:41 +0300

    feat: adding cart item logic

---
+++ lib/providers/cart.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.quantity,
+  });
+}
+
+class Cart with ChangeNotifier {
+  //list where the key is the product id and the value is a type of cart item
+  final Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get getItems {
+    return {..._items};
+  }
+
+  void addProductToCart(
+    String productId,
+    String title,
+    double price,
+  ) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCart) => CartItem(
+              id: existingCart.id,
+              title: existingCart.title,
+              price: existingCart.price,
+              quantity: existingCart.quantity + 1));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              price: price,
+              quantity: 1));
+    }
+  }
+}
