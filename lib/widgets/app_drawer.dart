8fcafab9792b1c1bf61c071c8e808804fa601dd8
Fady Moukawam <famoukawam@alinma.com>
2023-06-13 15:20:45 +0300

    feat: AppDrawer added

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/order_screen.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          // DrawerHeader(
+          //   decoration: BoxDecoration(
+          //     color: Theme.of(context).colorScheme.primaryContainer,
+          //   ),
+          //   child: const Text(
+          //     'Menu',
+          //     style: TextStyle(
+          //       color: Colors.white,
+          //       fontSize: 24,
+          //     ),
+          //   ),
+          // ),
+          AppBar(
+            title: const Text('Menu'),
+            automaticallyImplyLeading: false,
+          ),
+          ListTile(
+            leading: const Icon(Icons.shop),
+            title: const Text('Shop'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.payment),
+            title: const Text('Orders'),
+            onTap: () {
+              Navigator.of(context).pushNamed(OrderScreen.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
