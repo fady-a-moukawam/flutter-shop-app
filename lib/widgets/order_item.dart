354310301e71bb96d2e4d707725eb81ee54db29e
Fady Moukawam <famoukawam@alinma.com>
2023-06-13 16:42:09 +0300

    fix: add order summary

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:math';
 
 import 'package:shop_app/providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   const OrderItem({super.key, required this.order});
+
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _isOpen = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +23,35 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle: Text(DateFormat.MMMEd().format(order.dateTime)),
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(DateFormat.MMMEd().format(widget.order.dateTime)),
             trailing: IconButton(
-              icon: const Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_isOpen ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _isOpen = !_isOpen;
+                });
+              },
             ),
-          )
+          ),
+          if (_isOpen)
+            Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                height: min(widget.order.products.length * 20.0 + 100, 100),
+                child: ListView.builder(
+                  itemBuilder: (_, index) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(widget.order.products[index].title),
+                        Text(
+                            '\$${widget.order.products[index].quantity}x ${widget.order.products[index].price}')
+                      ],
+                    );
+                  },
+                  itemCount: widget.order.products.length,
+                ))
         ],
       ),
     );
