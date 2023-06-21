3414426cedcb23e5448142bcea2057a6bfada09a
Fady Moukawam <famoukawam@alinma.com>
2023-06-21 11:39:59 +0300

    feat: add actions for item

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
+
+import 'package:shop_app/screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
 
   const UserProductItem(
-      {super.key, required this.title, required this.imageUrl});
+      {super.key,
+      required this.title,
+      required this.imageUrl,
+      required this.id});
 
   @override
   Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context, listen: false);
+
     return Card(
       child: ListTile(
         leading: CircleAvatar(backgroundImage: NetworkImage(imageUrl)),
@@ -16,9 +26,16 @@ class UserProductItem extends StatelessWidget {
         trailing: SizedBox(
           width: 100,
           child: Row(children: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushNamed(EditProductScreen.routeName, arguments: id);
+                },
+                icon: const Icon(Icons.edit)),
+            IconButton(
+                onPressed: () {
+                  productsData.deleteProduct(id);
+                },
                 icon: const Icon(
                   Icons.delete,
                   color: Colors.red,
