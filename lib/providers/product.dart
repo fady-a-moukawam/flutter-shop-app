c274ebcee8d37b6e0deacf54689cd5e6490d1146
Fady Moukawam <famoukawam@alinma.com>
2023-07-09 16:54:56 +0300

    feat: handle mark as favorite from be

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,8 +18,27 @@ class Product with ChangeNotifier {
       required this.imageUrl,
       this.isFavorite = false});
 
-  void toggleFavorite() {
+  void _rollbackMarkIsFavorite(bool updatedVal) {
+    isFavorite = updatedVal;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavorite() async {
+    bool oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+
+    try {
+      final url = Uri.parse(
+          'https://flutter-shop-app-1f18f-default-rtdb.firebaseio.com/products/$id.json');
+      final res =
+          await http.patch(url, body: json.encode({'isFavorite': isFavorite}));
+
+      if (res.statusCode >= 400) {
+        _rollbackMarkIsFavorite(oldStatus);
+      }
+    } catch (error) {
+      _rollbackMarkIsFavorite(oldStatus);
+    }
   }
 }
