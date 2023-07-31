205d71ecab4bd4734e814090ac61b90f344d3b87
Fady Moukawam <famoukawam@alinma.com>
2023-07-31 12:49:41 +0300

    feat: add logout

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -64,4 +64,11 @@ class Auth with ChangeNotifier {
   Future<void> signIn(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
+
+  void logout() {
+    _token = null;
+    _expiryDate = null;
+    _userId = null;
+    notifyListeners();
+  }
 }
