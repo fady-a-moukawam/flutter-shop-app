8d4bf87a47b05ef91a17bc58a0a8e11aa0cc108f
Fady Moukawam <famoukawam@alinma.com>
2023-08-06 15:47:36 +0300

    feat: animated order summary container

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -34,24 +34,26 @@ class _OrderItemState extends State<OrderItem> {
               },
             ),
           ),
-          if (_isOpen)
-            Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                height: min(widget.order.products.length * 20.0 + 100, 100),
-                child: ListView.builder(
-                  itemBuilder: (_, index) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(widget.order.products[index].title),
-                        Text(
-                            '\$${widget.order.products[index].quantity}x ${widget.order.products[index].price}')
-                      ],
-                    );
-                  },
-                  itemCount: widget.order.products.length,
-                ))
+          AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.decelerate,
+              height: _isOpen
+                  ? min(widget.order.products.length * 20.0 + 100, 100)
+                  : 0,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: ListView.builder(
+                itemBuilder: (_, index) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(widget.order.products[index].title),
+                      Text(
+                          '\$${widget.order.products[index].quantity}x ${widget.order.products[index].price}')
+                    ],
+                  );
+                },
+                itemCount: widget.order.products.length,
+              ))
         ],
       ),
     );
